0828d6649fd0348876f0132fe15034743b013e79
L€0 <69213766+LeoGBK@users.noreply.github.com>
2025-02-27 10:13:22 +0100

    Checking popup display and testing buttons

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const producto = document.querySelectorAll('.tarjeta');
 const popup = document.getElementById('popup');
-const aviso = document.getElementById('.aviso');
+const aviso = document.getElementById('aviso');
 
 producto.forEach(producto => {
     producto.addEventListener('click', ()=> {
         aviso.textContent = `Producto agregado al carrito: ${producto.querySelector('h2').textContent}`;
-        
+        popup.style.display = 'block'; /* Making popup to be displayed */
     })
 })
