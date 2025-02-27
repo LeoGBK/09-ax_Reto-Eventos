7aedf3ef8a3649ec5b6e12c494cc4bcc464949c9
L€0 <69213766+LeoGBK@users.noreply.github.com>
2025-02-27 11:30:44 +0100

    Including DOMContentLoaded event

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const producto = document.querySelectorAll('.tarjeta');
-const popup = document.getElementById('popup');
-const aviso = document.getElementById('aviso');
+// Including DOMContentLoaded event listener so it works along "defer" attibute in HTML file.
+document.addEventListener('DOMContentLoaded', () => {
+    const producto = document.querySelectorAll('.tarjeta');
+    const popup = document.getElementById('popup');
+    const aviso = document.getElementById('aviso');
 
-producto.forEach(producto => {
-    producto.addEventListener('click', () => {
-        aviso.textContent = `Producto agregado al carrito: ${producto.querySelector('h2').textContent}`;
-        popup.style.display = 'block'; // Making popup to be displayed.
-        // Making the following instruction to execute after 3 seconds.
-        setInterval(() => {
-            popup.style.display = 'none';
-        }, 5000)
+    producto.forEach(producto => {
+        producto.addEventListener('click', () => {
+            aviso.textContent = `Producto agregado al carrito: ${producto.querySelector('h2').textContent}`;
+            popup.style.display = 'block'; // Making popup to be displayed.
+            // Making the following instruction to execute after 3 seconds.
+            setInterval(() => {
+                popup.style.display = 'none';
+            }, 3000)
+        })
     })
 })
