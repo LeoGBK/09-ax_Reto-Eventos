d7a7a9332040f8d2bd9f7a139cf37c1fba960948
L€0 <69213766+LeoGBK@users.noreply.github.com>
2025-02-27 11:09:39 +0100

    Coding interval and fixing navigation menu

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const popup = document.getElementById('popup');
 const aviso = document.getElementById('aviso');
 
 producto.forEach(producto => {
-    producto.addEventListener('click', ()=> {
+    producto.addEventListener('click', () => {
         aviso.textContent = `Producto agregado al carrito: ${producto.querySelector('h2').textContent}`;
-        popup.style.display = 'block'; /* Making popup to be displayed */
+        popup.style.display = 'block'; // Making popup to be displayed.
+        // Making the following instruction to execute after 3 seconds.
+        setInterval(() => {
+            popup.style.display = 'none';
+        }, 5000)
     })
 })
